5639160d1b04a8f116cee4fa5a85dababdb1c976
hungryee <stas@rocks.gold>
2023-01-23 21:43:45 +0100

    kedro pipeline creation

--- bitcoin-price-prediction/src/bitcoin_price_prediction/pipelines/process_data/nodes.py
+++ bitcoin-price-prediction/src/bitcoin_price_prediction/pipelines/process_data/nodes.py
@@ -2,3 +2,29 @@
 This is a boilerplate pipeline 'process_data'
 generated using Kedro 0.18.3
 """
+import datetime
+
+import pandas as pd
+
+
+def drop_unused(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.drop(columns=['Volume_(BTC)', 'Volume_(Currency)', 'Weighted_Price'])
+    return df
+
+def timestamp_to_datetime_indexed(df: pd.DataFrame) -> pd.DataFrame:
+    df['Timestamp'] = df['Timestamp'].apply(lambda x: datetime.datetime.fromtimestamp(x))
+    df = df.set_index(keys='Timestamp')
+    return df
+
+
+def convert_to_15min(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.groupby(pd.Grouper(freq='15min')).agg(
+        {"Open": "first", "High": "max", "Low": "min", "Close": "last"})
+    return df
+
+if __name__ == '__main__':
+    df = pd.read_csv('/Users/hungryee/PycharmProjects/ASI_GROUP_PROJECT/bitcoin-price-prediction/data/bitcoin_data/historical_price_1min.csv')
+    df = drop_unused(df)
+    df = timestamp_to_datetime_indexed(df)
+    df = convert_to_15min(df)
+    pass
--- bitcoin-price-prediction/src/bitcoin_price_prediction/pipelines/process_data/pipeline.py
+++ bitcoin-price-prediction/src/bitcoin_price_prediction/pipelines/process_data/pipeline.py
@@ -4,13 +4,32 @@ generated using Kedro 0.18.3
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-
+from .nodes import *
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+            drop_unused,
+            inputs='bitcoin_historical_data',
+            outputs='with_dropped_columns',
+            name='drop_unused_node'
+        ),
+        node(
+            timestamp_to_datetime_indexed,
+            inputs='with_dropped_columns',
+            outputs='with_datetime',
+            name='timestamp_to_datetime_node'
+        ),
+        node(
+            convert_to_15min,
+            inputs='with_datetime',
+            outputs='with_15min_timeframe',
+            name='convert_to_15min_node'
+        )
+    ])
 
 #todo
-#dvc
+#--dvc
 #kedro
 #ansible
 #fastapi
